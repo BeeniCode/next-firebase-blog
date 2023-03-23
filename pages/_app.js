a6b6e1e4a55313877d9afbd4ab528e43fff61d02
Beeni <beeni4429@gmail.com>
2023-03-23 15:53:52 +0500

    final

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import Footer from "@/components/footer";
+import Navbar from "@/components/navbar";
+import "@/styles/globals.css";
+import { MyProvider } from "@/context/MyContext";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MyProvider>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </MyProvider>
+  );
 }
